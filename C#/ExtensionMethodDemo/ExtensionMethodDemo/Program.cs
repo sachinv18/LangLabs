f851bc558888a27c0b6d1346db00ed5ffb1905bc
sachin.vastrad <sachin.vastrad@ivanti.com>
2018-04-20 07:01:35 +0530

    lang Extension Demo
    lang Extension Demo

---
+++ C#/ExtensionMethodDemo/ExtensionMethodDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethodDemo;
+namespace ExtensionMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "12.33";
+            Console.WriteLine(str.MyToDouble());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C#/ExtensionMethodDemo/ExtensionMethodDemo/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodDemo
+{
+    public static class StringExtensions
+    {
+        public static double MyToDouble (this string data)
+        {
+        
+            return Double.Parse(data);
+            
+        }
+    }
+}
