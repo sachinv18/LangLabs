02e39fface8281e2af137b696311a5eff54f3db8
sachin.vastrad <sachin.vastrad@ivanti.com>
2018-04-20 11:55:24 +0530

    Lamda Expression and Var
    Lamda Expression and Var

---
+++ C#/LamdaExpressionDemo/LamdaExpressionDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Implicit Typing 
+namespace LamdaExpressionDemo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //var
+            var s = "sachin";
+            var i = 10;
+            var d = 3.4;
+            Console.WriteLine("{0},{1},{2}", s, 1, d);
+
+
+
+           Func<int,int> square = x => x * x;
+
+            Func<int, int, int> sum = (x, y) =>
+             {
+                 int z;
+                 z = x + y;
+                 return z;
+             };
+
+            //when no parameter or no return type then use Action
+            Action greet1 = () => Console.WriteLine("Hello*World");
+
+            Action <String> greet = x =>
+            {
+                if(x.Length >0)
+                Console.WriteLine("Hello {0}", x);
+                else
+                    Console.WriteLine("Hello World");
+            };
+            Console.WriteLine(square(5));
+            Console.WriteLine(sum(4, 5));
+            greet("Sachin");
+            greet("");
+            greet1();
+
+
+            SayHelloWithAddion(x =>
+            {
+                if (x.Length > 0)
+                    Console.WriteLine("Hello {0}", x);
+                else
+                    Console.WriteLine("Hello World");
+            });
+
+
+
+        }
+
+        private static void SayHelloWithAddion(Action<String> will)
+        {
+            will(" Rangoon");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/LamdaExpressionDemo/LamdaExpressionDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LamdaExpressionDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ivanti")]
+[assembly: AssemblyProduct("LamdaExpressionDemo")]
+[assembly: AssemblyCopyright("Copyright © Ivanti 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a6217369-4ae0-4ec8-91a1-f919a7628124")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
